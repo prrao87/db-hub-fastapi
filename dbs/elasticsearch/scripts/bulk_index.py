f86f5d57881fb3c3fcdc36b2a79693bb98bea9e0
Lint Action <lint-action@samuelmeuli.com>
2023-04-15 15:26:32 +0000

    Fix code style issues with Black

--- dbs/elasticsearch/scripts/bulk_index.py
+++ dbs/elasticsearch/scripts/bulk_index.py
@@ -161,7 +161,9 @@ async def main(files: list[str]) -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Bulk index database from the wine reviews JSONL data")
+    parser = argparse.ArgumentParser(
+        "Bulk index database from the wine reviews JSONL data"
+    )
     parser.add_argument(
         "--limit",
         type=int,
