bad845fb0528d21a21a77a47d08c149c89210d0f
prrao87 <prrao87@gmail.com>
2023-04-19 11:04:42 -0400

    FastAPI app to query qdrant

---
+++ dbs/qdrant/api/main.py
@@ -0,0 +1,48 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from functools import lru_cache
+
+from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient
+
+from api.config import Settings
+from api.routers.wine import wine_router
+
+@lru_cache()
+def get_settings():
+    # Use lru_cache to avoid loading .env file for every request
+    return Settings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """Async context manager for Qdrant database connection."""
+    settings = get_settings()
+    model_checkpoint = settings.embedding_model_checkpoint
+    app.model = SentenceTransformer(model_checkpoint, device="cpu")
+    app.client = QdrantClient(host=settings.qdrant_service, port=settings.qdrant_port)
+    print("Successfully connected to Qdrant")
+    yield
+    print("Successfully closed Qdrant connection and released resources")
+
+
+app = FastAPI(
+    title="REST API for wine reviews on Qdrant",
+    description=(
+        "Query from a Qdrant database of 130k wine reviews from the Wine Enthusiast magazine"
+    ),
+    version=get_settings().tag,
+    lifespan=lifespan,
+)
+
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return {
+        "message": "REST API for querying Qdrant database of 130k wine reviews from the Wine Enthusiast magazine"
+    }
+
+
+# Attach routes
+app.include_router(wine_router, prefix="/wine", tags=["wine"])
