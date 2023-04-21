1035a6eabf158d7a37a32862b68c1cb10ab34e78
prrao87 <prrao87@gmail.com>
2023-04-21 12:47:02 -0400

    Update meilisearch bulk indexing to use srsly

--- dbs/meilisearch/scripts/bulk_index.py
+++ dbs/meilisearch/scripts/bulk_index.py
@@ -2,20 +2,19 @@ from __future__ import annotations
 
 import argparse
 import asyncio
-import glob
 import json
 import os
 import sys
-import zipfile
 from concurrent.futures import ProcessPoolExecutor
 from functools import lru_cache, partial
 from pathlib import Path
-from typing import Any
+from typing import Any, Iterator
 
+import srsly
 from dotenv import load_dotenv
 from meilisearch_python_async import Client
-from meilisearch_python_async.models.settings import MeilisearchSettings
 from meilisearch_python_async.index import Index
+from meilisearch_python_async.models.settings import MeilisearchSettings
 from pydantic.main import ModelMetaclass
 
 sys.path.insert(1, os.path.realpath(Path(__file__).resolve().parents[1]))
@@ -40,46 +39,25 @@ def get_settings():
     return Settings()
 
 
-def get_json_files(file_prefix: str, file_path: Path, data_dir: Path) -> list[str]:
+def chunk_iterable(item_list: list[JsonBlob], chunksize: int) -> Iterator[tuple[JsonBlob, ...]]:
+    """
+    Break a large iterable into an iterable of smaller iterables of size `chunksize`
+    """
+    for i in range(0, len(item_list), chunksize):
+        yield tuple(item_list[i : i + chunksize])
+
+
+def get_json_data(data_dir: Path, filename: str) -> list[JsonBlob]:
     """Get all line-delimited json files (.jsonl) from a directory with a given prefix"""
-    files = sorted(glob.glob(f"{file_path}/{file_prefix}*.jsonl"))
-    # Files may not have been unzipped yet so try to do that first
-    if not files:
-        extract_json_from_zip(data_dir, file_path)
-        # Now try to get the files again after unzipping
-        files = sorted(glob.glob(f"{file_path}/{file_prefix}*.jsonl"))
-        # This time if they aren't there they really don't exist
-        if not files:
-            raise FileNotFoundError(
-                f"No .jsonl files with prefix `{file_prefix}` found in `{file_path}`"
-            )
-    return files
-
-
-def clean_directory(dirname: Path) -> None:
-    """Clean up existing files to avoid conflicts"""
-    if Path(dirname).exists():
-        for f in Path(dirname).glob("*"):
-            if f.is_file():
-                f.unlink()
-
-
-def extract_json_from_zip(data_path: Path, file_path: Path) -> None:
-    """
-    Extract .jsonl files from zip file and save them in `file_path`
-    """
-    clean_directory(file_path)
-    zipfiles = sorted(glob.glob(f"{str(data_path)}/*.zip"))
-    for file in zipfiles:
-        with zipfile.ZipFile(file, "r") as zipf:
-            for fn in zipf.infolist():
-                fn.filename = Path(fn.filename).name
-                zipf.extract(fn, file_path)
-
-
-def read_jsonl_from_file(filename: str) -> list[JsonBlob]:
-    with open(filename, "r") as f:
-        data = [json.loads(line.strip()) for line in f.readlines()]
+    file_path = data_dir / filename
+    if not file_path.is_file():
+        # File may not have been uncompressed yet so try to do that first
+        data = srsly.read_gzip_jsonl(file_path)
+        # This time if it isn't there it really doesn't exist
+        if not file_path.is_file():
+            raise FileNotFoundError(f"No valid .jsonl file found in `{data_dir}`")
+    else:
+        data = srsly.read_gzip_jsonl(file_path)
     return data
 
 
@@ -92,10 +70,9 @@ def validate(
     return validated_data
 
 
-def process_file(file_name: str) -> tuple[list[JsonBlob], str]:
-    data = read_jsonl_from_file(file_name)
+def process_file(data: list[JsonBlob]) -> tuple[list[JsonBlob], str]:
     validated_data = validate(data, Wine, exclude_none=True)
-    return validated_data, file_name
+    return validated_data
 
 
 def get_meili_settings(filename: str) -> MeilisearchSettings:
@@ -108,14 +85,16 @@ def get_meili_settings(filename: str) -> MeilisearchSettings:
 
 # --- Async functions ---
 
-async def update_documents_to_index(
-    index: Index, primary_key: str, data: list[JsonBlob], file_name: str
-) -> None:
+
+async def update_documents_to_index(index: Index, primary_key: str, data: list[JsonBlob]) -> None:
+    ids = [item[primary_key] for item in data]
     await index.update_documents(data, primary_key)
-    print(f"Indexed {Path(file_name).name} to db")
+    print(f"Indexed {len(ids)} ids in range {min(ids)}-{max(ids)} to db")
 
 
-async def main(files: list[str], meili_settings: MeilisearchSettings) -> None:
+async def main(
+    chunked_data: Iterator[tuple[JsonBlob, ...]], meili_settings: MeilisearchSettings
+) -> None:
     settings = Settings()
     URI = f"http://{settings.meili_url}:{settings.meili_port}"
     MASTER_KEY = settings.meili_master_key
@@ -132,61 +111,40 @@ async def main(files: list[str], meili_settings: MeilisearchSettings) -> None:
         with ProcessPoolExecutor() as process_pool:
             # Attach process pool to running event loop so that we can process multiple files in parallel
             loop = asyncio.get_running_loop()
-            calls = [partial(process_file, file_name) for file_name in files]
+            calls = [partial(process_file, chunk) for chunk in chunked_data]
             call_coroutines = [loop.run_in_executor(process_pool, call) for call in calls]
             # Gather and run document update coroutines
             coroutines = await asyncio.gather(*call_coroutines)
-            tasks = [
-                update_documents_to_index(index, primary_key, data, filename)
-                for data, filename in coroutines
-            ]
+            tasks = [update_documents_to_index(index, primary_key, data) for data in coroutines]
         try:
             await asyncio.gather(*tasks)
         except Exception as e:
             print(f"{e}: Error while indexing to db")
-    print(f"Finished indexing {len(files)} JSONL files to db")
 
 
 if __name__ == "__main__":
+    # fmt: off
     parser = argparse.ArgumentParser("Bulk index database from the wine reviews JSONL data")
-    parser.add_argument(
-        "--limit",
-        type=int,
-        default=0,
-        help="Limit the size of the dataset to load for testing purposes",
-    )
-    parser.add_argument(
-        "--refresh",
-        action="store_true",
-        help="Refresh zip file data by clearing existing directory & extracting them again",
-    )
-    parser.add_argument(
-        "--filename",
-        type=str,
-        default="winemag-data-130k-v2-jsonl.zip",
-        help="Name of the JSONL zip file to use",
-    )
+    parser.add_argument("--limit", type=int, default=0, help="Limit the size of the dataset to load for testing purposes")
+    parser.add_argument("--chunksize", type=int, default=10_000, help="Size of each chunk to break the dataset into before processing")
+    parser.add_argument("--filename", type=str, default="winemag-data-130k-v2.jsonl.gz", help="Name of the JSONL zip file to use")
     args = vars(parser.parse_args())
+    # fmt: on
 
     LIMIT = args["limit"]
     DATA_DIR = Path(__file__).parents[3] / "data"
-    ZIPFILE = DATA_DIR / args["filename"]
-    # Get file path for unzipped files
-    filename = Path(args["filename"]).stem
-    FILE_PATH = DATA_DIR / filename
+    FILENAME = args["filename"]
+    CHUNKSIZE = args["chunksize"]
 
-    # Extract JSONL files from zip files if `--refresh` flag is set
-    if args["refresh"]:
-        # Extract all json files from zip files from their parent directory and save them in `parent_dir/data`.
-        extract_json_from_zip(DATA_DIR, FILE_PATH)
-
-    files = get_json_files("winemag-data", FILE_PATH, DATA_DIR)
-
-    assert files, f"No files found in {FILE_PATH}"
-
+    data = list(get_json_data(DATA_DIR, FILENAME))
     if LIMIT > 0:
-        files = files[:LIMIT]
+        data = data[:LIMIT]
 
-    meili_settings = get_meili_settings(filename="settings.json")
+    chunked_data = chunk_iterable(data, CHUNKSIZE)
+
+    meili_settings = get_meili_settings(filename="settings/settings.json")
+
     # Run main async event loop
-    asyncio.run(main(files, meili_settings))
+    if data:
+        asyncio.run(main(chunked_data, meili_settings))
+        print(f"Finished indexing {len(data)} JSONL files to db")
