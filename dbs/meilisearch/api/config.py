12f1b07a54879143412010547e2c7872538ce668
prrao87 <prrao87@gmail.com>
2023-04-28 16:23:24 -0400

    Improve variable naming for meilisearch scripts

--- dbs/meilisearch/api/config.py
+++ dbs/meilisearch/api/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     meili_service: str
     meili_master_key: str
-    meili_port: str
+    meili_port: int
     meili_url: str
     tag: str
 
