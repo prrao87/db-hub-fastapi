e8b36b241ad79074ec7ccf343c8dae730b7988fb
prrao87 <prrao87@gmail.com>
2023-07-14 16:49:01 -0400

    Update pydantic settings per v2

--- dbs/neo4j/api/config.py
+++ dbs/neo4j/api/config.py
@@ -1,12 +1,15 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="allow",
+    )
+
     neo4j_service: str
     neo4j_url: str
     neo4j_user: str
     neo4j_password: str
     tag: str
 
-    class Config:
-        env_file = ".env"
