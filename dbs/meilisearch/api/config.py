9a1e63e834d11d8125bbb6346905c9e1de4cc639
prrao87 <prrao87@gmail.com>
2023-07-15 18:25:00 -0400

    Use pydantic-settings

--- dbs/meilisearch/api/config.py
+++ dbs/meilisearch/api/config.py
@@ -1,12 +1,14 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="allow",
+    )
+
     meili_service: str
     meili_master_key: str
     meili_port: int
     meili_url: str
     tag: str
-
-    class Config:
-        env_file = ".env"
