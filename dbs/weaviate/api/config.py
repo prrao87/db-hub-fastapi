cce21f99f65d00b4180430b66b19237506cb9093
prrao87 <prrao87@gmail.com>
2023-07-20 10:52:03 -0400

    Use pydantic-settings

--- dbs/weaviate/api/config.py
+++ dbs/weaviate/api/config.py
@@ -1,15 +1,19 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="allow",
+    )
+
+
     weaviate_service: str
     weaviate_port: str
     weaviate_host: str
     weaviate_service: str
-    api_port = str
+    api_port: int
     embedding_model_checkpoint: str
     onnx_model_filename: str
     tag: str
 
-    class Config:
-        env_file = ".env"
