7181d24a83398ac548dd6d437b854375fa43dfb8
prrao87 <prrao87@gmail.com>
2023-07-26 21:54:31 -0400

    Use pydantic-settings in qdrant

--- dbs/qdrant/api/config.py
+++ dbs/qdrant/api/config.py
@@ -1,15 +1,17 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="allow",
+    )
+
     qdrant_service: str
     qdrant_port: str
     qdrant_host: str
     qdrant_service: str
-    api_port = str
+    api_port: str
     embedding_model_checkpoint: str
     onnx_model_filename: str
     tag: str
-
-    class Config:
-        env_file = ".env"
