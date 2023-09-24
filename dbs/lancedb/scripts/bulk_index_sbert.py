ffbb49e7714b274767ad546639fc9c7f5f29e80f
prrao87 <prrao87@gmail.com>
2023-09-23 22:35:16 -0400

    Fix LanceDB data loader

--- dbs/lancedb/scripts/bulk_index_sbert.py
+++ dbs/lancedb/scripts/bulk_index_sbert.py
@@ -1,23 +1,25 @@
 import argparse
 import os
 import sys
-import time
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from functools import lru_cache
+from math import e
 from pathlib import Path
 from typing import Any, Iterator
 
-import srsly
-from dotenv import load_dotenv
-from pydantic.main import ModelMetaclass
 import lancedb
+import pandas as pd
+import srsly
+from codetiming import Timer
+from dotenv import load_dotenv
+from lancedb.pydantic import pydantic_to_schema
 from tqdm import tqdm
 
-
 sys.path.insert(1, os.path.realpath(Path(__file__).resolve().parents[1]))
-from api.config import Settings
-from schemas.wine import Wine
 from sentence_transformers import SentenceTransformer
+
+from api.config import Settings
+from schemas.wine import LanceModelWine, Wine
 
 load_dotenv()
 # Custom types
@@ -58,80 +60,83 @@ def get_json_data(data_dir: Path, filename: str) -> list[JsonBlob]:
 
 def validate(
     data: list[JsonBlob],
-    model: ModelMetaclass,
     exclude_none: bool = False,
 ) -> list[JsonBlob]:
-    validated_data = [model(**item).dict(exclude_none=exclude_none) for item in data]
+    validated_data = [Wine(**item).model_dump(exclude_none=exclude_none) for item in data]
     return validated_data
 
 
-def vectorize_text(data_chunk: list[JsonBlob]) -> list[JsonBlob]:
-    data = validate(data_chunk, Wine, exclude_none=True)
+def embed_func(batch: list[str], model) -> list[list[float]]:
+    return [model.encode(sentence.lower()) for sentence in batch]
 
+
+def vectorize_text(data: list[JsonBlob]) -> list[LanceModelWine] | None:
     # Load a sentence transformer model for semantic similarity from a specified checkpoint
     model_id = get_settings().embedding_model_checkpoint
+    assert model_id, "Invalid embedding model checkpoint specified in .env file"
     MODEL = SentenceTransformer(model_id)
 
     ids = [item["id"] for item in data]
-    to_vectorize = [text.pop("to_vectorize") for text in data]
-    vectors = [MODEL.encode(text.lower(), batch_size=64).tolist() for text in to_vectorize]
+    to_vectorize = [text.get("to_vectorize") for text in data]
+    vectors = embed_func(to_vectorize, MODEL)
     try:
-        # Generate sentence embeddings on batch
-        data_chunk = [{"vector": vector, **d} for d, vector in zip(data, vectors)]
+        data_batch = [{**d, "vector": vector} for d, vector in zip(data, vectors)]
     except Exception as e:
-        print(f"{e}: Failed to index ID range {min(ids)}-{max(ids)}")
+        print(f"{e}: Failed to add ID range {min(ids)}-{max(ids)}")
         return None
-    return data_chunk
+    return data_batch
 
 
-def get_concat_vectors_and_data(validated_data: list[JsonBlob]) -> list[JsonBlob]:
-    vector_start_time = time.time()
-    print("Vectorizing data on multiple cores:")
+def embed_batches(tbl: str, validated_data: list[JsonBlob]) -> pd.DataFrame:
     with ProcessPoolExecutor(max_workers=WORKERS) as executor:
         chunked_data = chunk_iterable(validated_data, CHUNKSIZE)
-        final_data = []
+        embed_data = []
         for chunk in tqdm(chunked_data, total=len(validated_data) // CHUNKSIZE):
             futures = [executor.submit(vectorize_text, chunk)]
-            final_data.extend(f.result() for f in as_completed(futures) if f.result())
-    print(
-        f"Finished vectorizing {len(data)} texts in {time.time() - vector_start_time:.4f} seconds"
-    )
-    return final_data
+            embed_data = [f.result() for f in as_completed(futures) if f.result()][0]
+            df = pd.DataFrame.from_dict(embed_data)
+            tbl.add(df, mode="overwrite")
 
 
 def main(data: list[JsonBlob]) -> None:
-    settings = get_settings()
+    DB_NAME = "../lancedb"
     TABLE = "wines"
-    # Because LanceDB is serverless, we can simply specify a disk path
-    db_name = "lancedb"
-    db = lancedb.connect(f"{db_name}")
-    validated_data = validate(data, Wine, exclude_none=True)
+    db = lancedb.connect(DB_NAME)
 
-    try:
-        tbl = db.open_table(TABLE).head(1)
-    except:
-        # Create a LanceDB table if it doesn't already exist
-        print(f"No table named {TABLE} found, creating new table with data and vectors")
-        final_data = get_concat_vectors_and_data(validated_data)
+    tbl = db.create_table(TABLE, schema=pydantic_to_schema(LanceModelWine), mode="overwrite")
+    print(f"Created table `{TABLE}`, with length {len(tbl)}")
 
-        tbl = db.create_table(TABLE, data=final_data[0], mode="overwrite")
-        print(f"Created table `{tbl.name}`, with length {len(tbl)}")
+    with Timer(name="Bulk Index", text="Validated data using Pydantic in {:.4f} sec"):
+        validated_data = validate(data, exclude_none=False)
 
-    # Work with an existing table
+    with Timer(name="Embed batches", text="Created sentence embeddings in {:.4f} sec"):
+        embed_batches(tbl, validated_data)
+
+    print(f"Finished inserting {len(tbl)} items into LanceDB table")
+
+    with Timer(name="Create index", text="Created IVF-PQ index in {:.4f} sec"):
+        # Creating index (choose num partitions as a power of 2 that's closest to len(dataset) // 5000)
+        # In this case, we have 130k datapoints, so the nearest power of 2 is 130000//5000 ~ 32)
+        tbl.create_index(metric="cosine", num_partitions=32, num_sub_vectors=96)
+
+
+def query_func() -> None:
+    DB_NAME = "../lancedb"
+    TABLE = "wines"
+    db = lancedb.connect(DB_NAME)
     tbl = db.open_table(TABLE)
-    print(f"Opened table `{tbl.name}`, with length {len(tbl)}")
-
-    # Creating index (choose num partitions as a power of 2 that's closest to len(dataset) // 5000)
-    # In this case, we have 130k datapoints, so the nearest power of 2 is 130000//5000 ~ 32)
-    print("Creating index")
-    tbl.create_index(metric="cosine", num_partitions=32, num_sub_vectors=96)
+    print(len(tbl))
+    MODEL = SentenceTransformer(get_settings().embedding_model_checkpoint)
+    query = "tropical fruit and citrus"
+    query_vector = embed_func([query], MODEL)[0]
+    print(tbl.search(query_vector).limit(5).to_df()[["id", "title", "description", "_distance"]])
 
 
 if __name__ == "__main__":
     # fmt: off
     parser = argparse.ArgumentParser("Bulk index database from the wine reviews JSONL data")
     parser.add_argument("--limit", type=int, default=0, help="Limit the size of the dataset to load for testing purposes")
-    parser.add_argument("--chunksize", type=int, default=512, help="Size of each chunk to break the dataset into before processing")
+    parser.add_argument("--chunksize", type=int, default=1000, help="Size of each chunk to break the dataset into before processing")
     parser.add_argument("--filename", type=str, default="winemag-data-130k-v2.jsonl.gz", help="Name of the JSONL zip file to use")
     parser.add_argument("--workers", type=int, default=4, help="Number of workers to use for vectorization")
     args = vars(parser.parse_args())
@@ -144,8 +149,9 @@ if __name__ == "__main__":
     WORKERS = args["workers"]
 
     data = list(get_json_data(DATA_DIR, FILENAME))
+    assert data, "No data found in the specified file"
+    data = data[:LIMIT] if LIMIT > 0 else data
+    main(data)
+    print("Finished execution!")
 
-    if data:
-        data = data[:LIMIT] if LIMIT > 0 else data
-        main(data)
-        print("Finished execution!")
+    query_func()
