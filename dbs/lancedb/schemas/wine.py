f096b8b335a41a748b96827b7a55cf02620b3cdd
prrao87 <prrao87@gmail.com>
2023-09-23 22:35:28 -0400

    Update schemas

--- dbs/lancedb/schemas/wine.py
+++ dbs/lancedb/schemas/wine.py
@@ -1,27 +1,16 @@
-from pydantic import BaseModel, root_validator
+from typing import Optional
+
+from lancedb.pydantic import Vector
+from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 
 class Wine(BaseModel):
-    id: int
-    points: int
-    title: str
-    description: str | None
-    price: float | None
-    variety: str | None
-    winery: str | None
-    vineyard: str | None
-    country: str | None
-    province: str | None
-    region_1: str | None
-    region_2: str | None
-    taster_name: str | None
-    taster_twitter_handle: str | None
-
-    class Config:
-        extra = "allow"
-        allow_population_by_field_name = True
-        validate_assignment = True
-        schema_extra = {
+    model_config = ConfigDict(
+        populate_by_name=True,
+        validate_assignment=True,
+        extra="allow",
+        str_strip_whitespace=True,
+        json_schema_extra={
             "example": {
                 "id": 45100,
                 "points": 85,
@@ -38,17 +27,25 @@ class Wine(BaseModel):
                 "taster_name": "Michael Schachner",
                 "taster_twitter_handle": "@wineschach",
             }
-        }
+        },
+    )
 
-    @root_validator(pre=True)
-    def _get_vineyard(cls, values):
-        "Rename designation to vineyard"
-        vineyard = values.pop("designation", None)
-        if vineyard:
-            values["vineyard"] = vineyard.strip()
-        return values
+    id: int
+    points: int
+    title: str
+    description: Optional[str]
+    price: Optional[float]
+    variety: Optional[str]
+    winery: Optional[str]
+    vineyard: Optional[str] = Field(..., alias="designation")
+    country: Optional[str]
+    province: Optional[str]
+    region_1: Optional[str]
+    region_2: Optional[str]
+    taster_name: Optional[str]
+    taster_twitter_handle: Optional[str]
 
-    @root_validator
+    @model_validator(mode="before")
     def _fill_country_unknowns(cls, values):
         "Fill in missing country values with 'Unknown', as we always want this field to be queryable"
         country = values.get("country")
@@ -56,7 +53,7 @@ class Wine(BaseModel):
             values["country"] = "Unknown"
         return values
 
-    @root_validator
+    @model_validator(mode="before")
     def _add_to_vectorize_fields(cls, values):
         "Add a field to_vectorize that will be used to create sentence embeddings"
         variety = values.get("variety", "")
@@ -65,3 +62,47 @@ class Wine(BaseModel):
         to_vectorize = list(filter(None, [variety, title, description]))
         values["to_vectorize"] = " ".join(to_vectorize).strip()
         return values
+
+
+class LanceModelWine(BaseModel):
+    model_config = ConfigDict(
+        populate_by_name=True,
+        validate_assignment=True,
+        extra="allow",
+        str_strip_whitespace=True,
+        json_schema_extra={
+            "example": {
+                "id": 45100,
+                "points": 85,
+                "title": "Balduzzi 2012 Reserva Merlot (Maule Valley)",
+                "description": "Ripe in color and aromas, this chunky wine delivers heavy baked-berry and raisin aromas in front of a jammy, extracted palate. Raisin and cooked berry flavors finish plump, with earthy notes.",
+                "price": 10.0,
+                "variety": "Merlot",
+                "winery": "Balduzzi",
+                "vineyard": "Reserva",
+                "country": "Chile",
+                "province": "Maule Valley",
+                "region_1": "null",
+                "region_2": "null",
+                "taster_name": "Michael Schachner",
+                "taster_twitter_handle": "@wineschach",
+            }
+        },
+    )
+
+    id: int
+    points: int
+    title: str
+    description: Optional[str]
+    price: Optional[float]
+    variety: Optional[str]
+    winery: Optional[str]
+    vineyard: Optional[str] = Field(..., alias="designation")
+    country: Optional[str]
+    province: Optional[str]
+    region_1: Optional[str]
+    region_2: Optional[str]
+    taster_name: Optional[str]
+    taster_twitter_handle: Optional[str]
+    to_vectorize: str
+    vector: Vector(384)
